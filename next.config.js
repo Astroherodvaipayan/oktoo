bd8553bc88a552b22c9798af07308e04d503f066
pasho <brandon.aguilarc@gmail.com>
2024-10-10 10:04:02 -0600

    window issue

--- next.config.js
+++ next.config.js
@@ -1,78 +1,28 @@
-const { PHASE_PRODUCTION_BUILD } = require('next/constants');
-
 /** @type {import('next').NextConfig} */
-const nextConfig = (phase, { defaultConfig }) => {
-  if (phase === PHASE_PRODUCTION_BUILD || process.env.NEXT_PUBLIC_ENV == 'production') {
-    // Apply production-only configurations here
+const nextConfig = {
+  reactStrictMode: true,
+  trailingSlash: true,
+  experimental: {
+    appDir: true,
+    serverComponentsExternalPackages: ['@prisma/client'],
+  },
+  eslint: {
+    dirs: ['src'],
+    // Warning: This allows production builds to successfully complete even if
+    // your project has ESLint errors.
 
-    return {
-      // To enable a static export
-      output: 'export',
-      // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-      trailingSlash: true,
-      swcMinify: true,
-      // prevent double rerendering
-      reactStrictMode: false,
-      // remove logs, enable this on prod
-      compiler: {
-        removeConsole: {
-          exclude: ['error'],
-        },
-      },  
-      eslint: {
-        dirs: ["src"],
-        // Warning: This allows production builds to successfully complete even if
-        // your project has ESLint errors.
-    
-        // PLEASE GET BACK THIS TO FALSE ONCE THE TEST BUILD IS UP
-        ignoreDuringBuilds: true,
-      },
-      typescript: {
-        // !! WARN !!
-        // Dangerously allow production builds to successfully complete even if
-        // your project has type errors.
-        // !! WARN !!
-    
-        /// PLEASE FOR GODSEIK REMOVE THIS
-        ignoreBuildErrors: true,
-      },
-      experimental: {
-        appDir: true,
-        serverComponentsExternalPackages: ["@prisma/client", "docusign-esign"],
-      },
-    };
-  }
+    // PLEASE GET BACK THIS TO FALSE ONCE THE TEST BUILD IS UP
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // !! WARN !!
+    // Dangerously allow production builds to successfully complete even if
+    // your project has type errors.
+    // !! WARN !!
 
-  return {
-    // To enable a static export
-    output: 'export',
-    // Optional: Change links `/me` -> `/me/` and emit `/me.html` -> `/me/index.html`
-    trailingSlash: true,
-    swcMinify: true,
-    // prevent double rerendering
-    reactStrictMode: false,
-    experimental: {
-      appDir: true,
-      serverComponentsExternalPackages: ["@prisma/client", "docusign-esign"],
-    },
-    eslint: {
-      dirs: ["src"],
-      // Warning: This allows production builds to successfully complete even if
-      // your project has ESLint errors.
-  
-      // PLEASE GET BACK THIS TO FALSE ONCE THE TEST BUILD IS UP
-      ignoreDuringBuilds: true,
-    },
-    typescript: {
-      // !! WARN !!
-      // Dangerously allow production builds to successfully complete even if
-      // your project has type errors.
-      // !! WARN !!
-  
-      /// PLEASE FOR GODSEIK REMOVE THIS
-      ignoreBuildErrors: true,
-    },
-  };
+    /// PLEASE FOR GODSEIK REMOVE THIS
+    ignoreBuildErrors: true,
+  },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
