18e0828d37faf933e8831ff9571ac0b7bb5bacb3
pasho <brandon.aguilarc@gmail.com>
2024-10-21 07:03:16 -0600

    design

--- src/modules/token/components/token-summary-card.tsx
+++ src/modules/token/components/token-summary-card.tsx
@@ -1,5 +1,5 @@
 import { Button } from '@/components/ui/button';
-import RequestTokenSheet from '@/modules/transaction/request/request-token-sheet';
+import RequestTokenSheet, { ViewPortfolioSheet } from '@/modules/transaction/request/request-token-sheet';
 import { useState, useCallback } from 'react';
 import AllTokensSheet from './all-tokens-sheet';
 import DepositSheet from '@/modules/transaction/deposit/deposit-sheet';
@@ -9,7 +9,7 @@ import WithdrawTokenSheet from '@/modules/transaction/send/withdraw-token-sheet'
 const ActionButton = ({ title, emoji, onClick }: { title: string; emoji: string; onClick: () => void }) => {
   return (
     <div>
-      <Button onClick={onClick} variant="ghost" className="flex flex-col p-4 min-h-24 aspect-square">
+      <Button onClick={onClick} variant="ghost" className="flex flex-col p-0 min-h-16 aspect-square">
         <span className="text-2xl md:text-4xl">{emoji}</span>
         <span className="md:text-lg">{title}</span>
       </Button>
@@ -56,11 +56,12 @@ const TokenSummaryCard = ({
       <div className="text-zinc-400 text-lg">{subTitle || 'Account Balance'}</div>
       <div className="text-4xl md:text-6xl">{balance}</div>
       {!!bottomParagraph && <div className="text-zinc-400 text-lg">{bottomParagraph}</div>}
-      <div className="flex flex-row mt-2 md:mt-4 md:gap-2">
+      <div className="flex flex-row mt-2 md:mt-4 gap-1 md:gap-2">
         <ActionButton title="Deposit" emoji="📲" onClick={() => handleActionButtonClick('deposit')} />
         <ActionButton title="Send" emoji="🏌️" onClick={() => handleActionButtonClick('send')} />
         <ActionButton title="Withdraw" emoji="🤑" onClick={() => handleActionButtonClick('withdraw')} />
         <ActionButton title="Request" emoji="😇" onClick={() => handleActionButtonClick('request')} />
+        <ActionButton title="My Assets" emoji="💼" onClick={() => handleActionButtonClick('assets')} />
       </div>
 
       {/* Modals and Sheets */}
@@ -74,6 +75,10 @@ const TokenSummaryCard = ({
       <RequestTokenSheet
         open={currentTokenAction === 'request'}
         setOpen={(open) => (open ? setCurrentTokenAction('request') : closeAllSheets())}
+      />
+      <ViewPortfolioSheet
+        open={currentTokenAction === 'assets'}
+        setOpen={(open) => (open ? setCurrentTokenAction('assets') : closeAllSheets())}
       />
       <WithdrawTokenSheet
         tokenSymbol={currentTokenSymbol}
--- src/modules/transaction/request/request-token-sheet.tsx
+++ src/modules/transaction/request/request-token-sheet.tsx
@@ -1,13 +1,72 @@
+import { Button } from '@/components/ui/button';
+import { Input } from '@/components/ui/input';
 import { Sheet, SheetContent, SheetHeader, SheetTitle } from '@/components/ui/sheet';
+import { useAuthenticationStore } from '@/modules/authenticated/auth-store';
+import { PlusIcon } from 'lucide-react';
+import { useState } from 'react';
+
+const NewRequestTokenSheet = ({ open, setOpen }: { open: boolean; setOpen: (newValue: boolean) => void }) => {
+  return (
+    <Sheet open={open} onOpenChange={setOpen}>
+      <SheetContent side="bottom" className="bg-[#222] mx-auto">
+        <SheetHeader>
+          <SheetTitle>😇 New Token Request</SheetTitle>
+        </SheetHeader>
+        <div className="w-full space-y-4">
+          <Input name="title" placeholder="Title" />
+          <Input name="description" placeholder="Description" />
+          <Input name="emojis-separated-by-comma" placeholder="Emojis (comma-separated)" />
+          <Input name="ammount" placeholder="Amount" />
+          <Button className="w-full">Create</Button>
+        </div>
+      </SheetContent>
+    </Sheet>
+  );
+};
+export const ViewPortfolioSheet = ({ open, setOpen }: { open: boolean; setOpen: (newValue: boolean) => void }) => {
+  const { user } = useAuthenticationStore();
+  return (
+    <Sheet open={open} onOpenChange={setOpen}>
+      <SheetContent side="bottom" className="bg-[#222] mx-auto">
+        <SheetHeader>
+          <SheetTitle>💼 My assets</SheetTitle>
+        </SheetHeader>
+        <div className="w-full space-y-4 min-h-[60vh]">
+          <div className="w-full gap-2 flex flex-row mt-4">
+            <div>
+              <img src="https://img.cryptorank.io/coins/base1682407673437.png" className="w-6 h-6 round-full" />
+            </div>
+            <div className="flex-1 flex flex-col">
+              <b>{user.emojis}-emojipay.base.eth</b>
+              <span className="text-zinc-400">Handlename by base to identify your account</span>
+            </div>
+          </div>
+        </div>
+      </SheetContent>
+    </Sheet>
+  );
+};
 
 const RequestTokenSheet = ({ open, setOpen }: { open: boolean; setOpen: (newValue: boolean) => void }) => {
+  const [isNewRequestTokenOpen, setIsNewRequestTokenOpen] = useState(false);
+
+  const handleNewRequestOpen = () => {
+    setIsNewRequestTokenOpen(true);
+  };
+
   return (
     <Sheet open={open} onOpenChange={setOpen}>
       <SheetContent side="bottom" className="bg-[#222] mx-auto">
         <SheetHeader>
           <SheetTitle>😇 Request</SheetTitle>
         </SheetHeader>
-        <div className="w-full mt-4 flex flex-col">This feature is not available yet...</div>
+        <div className="w-full space-y-4 min-h-[60vh]">
+          <Button onClick={handleNewRequestOpen} className="mt-4 text-white ">
+            <PlusIcon /> Create new
+          </Button>
+        </div>
+
+        <NewRequestTokenSheet setOpen={setIsNewRequestTokenOpen} open={isNewRequestTokenOpen} />
       </SheetContent>
     </Sheet>
   );
